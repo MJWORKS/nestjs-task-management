00c85c8cf005133702155cf3dd4fd73814d9b031
MJWORKS <mjworks2020@outlook.com>
2022-05-30 17:44:25 -0300

    Created task model and enum

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,13 @@
 import { TasksService } from './tasks.service';
 import { Controller, Get } from '@nestjs/common';
+import { Task } from '../../dist/tasks/task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
 }
